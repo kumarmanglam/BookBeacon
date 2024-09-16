8944e9b60696c493d81700740256fa723318160d
kumarmanglam <kumarthe799@gmail.com>
2024-09-16 10:31:06 +0530

    feat: added view licenses page

--- BookBeacon_frontend/src/pages/ViewLicenses/index.tsx
+++ BookBeacon_frontend/src/pages/ViewLicenses/index.tsx
@@ -1,14 +1,19 @@
 
 import Table from "../../components/common/Table";
 import { licenses } from "./data"
-import { TABLE_HEADER_CONFIG } from "../../config/tableConfig";
+import { TABLE_HEADER_CONFIG, TABLE_HEADER_CONFIG_VIEW_lICENSE } from "../../config/tableConfig";
 import Navbar from "../../components/common/Navbar";
+import "./style.css"
 
 const ViewLicenses = () => {
     return (
         <div>
             <Navbar />
-            <Table headerConfig={TABLE_HEADER_CONFIG} data={licenses[2].booksInBundle} />
+            <div className="view-license-container">
+                <div className="table-container">
+                    <Table headerConfig={TABLE_HEADER_CONFIG_VIEW_lICENSE} data={licenses} />
+                </div>
+            </div>
         </div>
     )
 }
