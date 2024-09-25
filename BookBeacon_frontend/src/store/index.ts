f0944c17192502313270f0060ad626c7ea607ec0
Anish Kumar <anish.sde1@gmail.com>
2024-09-25 17:25:27 +0530

    updated create License

--- BookBeacon_frontend/src/store/index.ts
+++ BookBeacon_frontend/src/store/index.ts
@@ -1,2 +1,11 @@
+import { configureStore, createStore } from "@reduxjs/toolkit";
+import LicenseReducer from "./reducers/License.reducer";
 
+const store = configureStore({
+    reducer: {
+        license: LicenseReducer,
+    }
+}
+)
 
+export default store;
--- BookBeacon_frontend/src/store/selectors/License.selector.ts
+++ BookBeacon_frontend/src/store/selectors/License.selector.ts
@@ -1,1 +1,5 @@
+import { createSelector } from "@reduxjs/toolkit";
 
+const selectLicenseStore = (state: any) => state.license;
+
+export const selectLicenseState = createSelector(selectLicenseStore, (state: any) => state.license)
