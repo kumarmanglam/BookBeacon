e21f79913d4634cd34f6ba9c894de12b52c0d3d9
kumarmanglam <kumarthe799@gmail.com>
2024-09-26 12:21:26 +0530

    fix: fixed table component

--- BookBeacon_frontend/src/components/common/Table/index.tsx
+++ BookBeacon_frontend/src/components/common/Table/index.tsx
@@ -25,113 +25,118 @@ const Table: React.FC<TableProps> = ({ headerConfig, data }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-    function callSetUpdateLicense(id: number, concurrency: number) {
-        const data = {
-            id: id,
-            concurrency: concurrency
-        }
-        dispatch(updateLicenseBooksInBundle(data));
+  function callSetUpdateLicense(id: number, concurrency: number) {
+    const data = {
+      id: id,
+      concurrency: concurrency
     }
-    function handleBlur() {
+    dispatch(updateLicenseBooksInBundle(data));
+  }
+  function handleBlur() {
 
-    }
-    return (
-        <table className="table">
-            <thead>
-                <tr className="table-row table-head">
-                    {
-                        headerConfig.map((item: any, index: number) => {
-                            return <th key={index} className={`${item.classes} table-head-data table-data`}>{item.label}</th>
-                        })
-                    }
-                </tr>
+  }
+  return (
+    <table className="table">
+      <thead>
+        <tr className="table-row table-head">
+          {
+            headerConfig.map((item: any, index: number) => {
+              return <th key={index} className={`${item.classes} table-head-data table-data`}>{item.label}</th>
+            })
+          }
+        </tr>
 
-            </thead>
-            <tbody>
-                {
-                    data.map((item: any, index: number) => (
-                        <tr key={index} className="table-row">
-                            {
-                                headerConfig.map((val: any, i: any) => {
-                                    const cellData = item[val["key"]];
-                                    if (val.key === 'edit') {
-                                        return (
-                                            <td key={i} className={`${val.classes}  table-data`}>
-                                                <button onClick={() => navigate("/license")}><FontAwesomeIcon icon={faEdit} /></button>
-                                            </td>
-                                        );
-                                    } if (val.key === 'concurrency') {
-                                        return (
-                                            <td key={i} className={`${val.classes} table-data`}>
-                                                <input
-                                                    type="number"
-                                                    value={item.concurrency}
-                                                    className="concurrency-input"
-                                                    onBlur={(e: any) => {
-                                                        if (e.target.value == "") {
-                                                            callSetUpdateLicense(item.book_id, 0)
-                                                        }
-                                                    }}
-                                                    onChange={(e: any) => callSetUpdateLicense(item.book_id, e.target.value)}
-                                                />
-                                            </td>
-                                        );
-                                    }
-                if (val.key === 'license_name') {
+      </thead>
+      <tbody>
+        {data.length > 0 ? (
+          data.map((item: any, index: number) => (
+            <tr key={index} className="table-row">
+              {
+                headerConfig.map((val: any, i: any) => {
+                  const cellData = item[val["key"]];
+                  if (val.key === 'edit') {
+                    return (
+                      <td key={i} className={`${val.classes}  table-data`}>
+                        <button onClick={() => navigate("/license")}><FontAwesomeIcon icon={faEdit} /></button>
+                      </td>
+                    );
+                  } if (val.key === 'concurrency') {
+                    return (
+                      <td key={i} className={`${val.classes} table-data`}>
+                        <input
+                          type="number"
+                          value={item.concurrency}
+                          className="concurrency-input"
+                          onBlur={(e: any) => {
+                            if (e.target.value == "") {
+                              callSetUpdateLicense(item.book_id, 0)
+                            }
+                          }}
+                          onChange={(e: any) => callSetUpdateLicense(item.book_id, e.target.value)}
+                        />
+                      </td>
+                    );
+                  }
+                  if (val.key === 'license_name') {
+                    return (
+                      <td key={i} className={`${val.classes} table-data`}>
+                        {cellData}
+                      </td>
+                    );
+                  }
+                  if (val.key === 'mode') {
+                    return (
+                      <td key={i} className={`${val.classes} table-data`}>
+                        {cellData}
+                      </td>
+                    );
+                  }
+                  if (val.key === 'start_date') {
+                    return (
+                      <td key={i} className={`${val.classes} table-data`}>
+                        {cellData}
+                      </td>
+                    );
+                  }
+                  if (val.key === 'end_date') {
+                    return (
+                      <td key={i} className={`${val.classes} table-data`}>
+                        {cellData}
+                      </td>
+                    );
+                  }
+                  if (val.key === 'edit') {
+                    return (
+                      <td key={i} className={`${val.classes} table-data`}>
+                        <button onClick={() => navigate('/booksInLicense')}>
+                          <FontAwesomeIcon icon={faEdit} />
+                        </button>
+                      </td>
+                    );
+                  }
+                  if (val.key === 'concurrency') {
+                    return (
+                      <td key={i} className={`${val.classes} table-data`}>
+                        <input
+                          type="number"
+                          value={item.concurrency}
+                          className="concurrency-input"
+                          onBlur={(e: any) => {
+                            if (e.target.value == "") {
+                              callSetUpdateLicense(item.book_id, 0);
+                            }
+                          }}
+                          onChange={(e) => callSetUpdateLicense(item.book_id, parseInt(e.target.value))}
+                        />
+                      </td>
+                    );
+                  }
                   return (
                     <td key={i} className={`${val.classes} table-data`}>
-                      {cellData}
+                      {cellData ?? 'N/A'}
                     </td>
                   );
-                }
-                if (val.key === 'mode') {
-                  return (
-                    <td key={i} className={`${val.classes} table-data`}>
-                      {cellData}
-                    </td>
-                  );
-                }
-                if (val.key === 'start_date') {
-                  return (
-                    <td key={i} className={`${val.classes} table-data`}>
-                      {cellData}
-                    </td>
-                  );
-                }
-                if (val.key === 'end_date') {
-                  return (
-                    <td key={i} className={`${val.classes} table-data`}>
-                      {cellData}
-                    </td>
-                  );
-                }
-                if (val.key === 'edit') {
-                  return (
-                    <td key={i} className={`${val.classes} table-data`}>
-                      <button onClick={() => navigate('/booksInLicense')}>
-                        <FontAwesomeIcon icon={faEdit} />
-                      </button>
-                    </td>
-                  );
-                }
-                if (val.key === 'concurrency') {
-                  return (
-                    <td key={i} className={`${val.classes} table-data`}>
-                      <input
-                        type="number"
-                        value={item.concurrency}
-                        className="concurrency-input"
-                        onChange={(e) => handleConcurrencyChange(item.book_id, parseInt(e.target.value))}
-                      />
-                    </td>
-                  );
-                }
-                return (
-                  <td key={i} className={`${val.classes} table-data`}>
-                    {cellData ?? 'N/A'}
-                  </td>
-                );
-              })}
+                })}
             </tr>
           ))
         ) : (
