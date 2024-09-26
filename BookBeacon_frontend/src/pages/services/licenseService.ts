a21b19e4e5bad4f04b8d78879eccb50bad540981
Akanksha <akanksha.pal@triconinfotech.com>
2024-09-26 16:40:56 +0530

    modified table

--- BookBeacon_frontend/src/pages/services/licenseService.ts
+++ BookBeacon_frontend/src/pages/services/licenseService.ts
@@ -3,16 +3,20 @@ import axios from 'axios';
 export const fetchLicenses = async () => {
   try {
     const response = await axios.get('http://localhost:3000/licenses');
-    
-    const filteredData = response.data.map(({ license_name, mode,start_date,end_date}: { license_name: string; mode: string ;start_date:string;end_date:string}) => ({
-      license_name,
-      mode,
-      start_date:start_date.slice(0, 10),  
-      end_date:end_date.slice(0, 10),  
-    }));
 
-    console.log(filteredData); 
-    return filteredData; 
+    const filteredData = response.data.map((license: { license_name: string; mode: string; start_date: string | null; end_date: string | null }) => {
+      const { license_name, mode, start_date, end_date } = license;
+      
+      return {
+        license_name,
+        mode,
+        start_date: start_date ? start_date.slice(0, 10) : 'N/A',  // Check if start_date exists before slicing
+        end_date: end_date ? end_date.slice(0, 10) : 'N/A',        // Check if end_date exists before slicing
+      };
+    });
+
+    console.log(filteredData);
+    return filteredData;
   } catch (error) {
     console.error('Error fetching licenses:', error);
     return [];
