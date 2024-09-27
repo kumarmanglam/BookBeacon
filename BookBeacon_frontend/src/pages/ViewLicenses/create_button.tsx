52fb8818d8580aedfe0d0e512705837b87e56419
kumarmanglam <kumarthe799@gmail.com>
2024-09-27 16:27:48 +0530

    fix: fixed login page

--- BookBeacon_frontend/src/pages/ViewLicenses/create_button.tsx
+++ BookBeacon_frontend/src/pages/ViewLicenses/create_button.tsx
@@ -54,9 +54,9 @@ const ButtonGroup: React.FC = () => {
       <button onClick={() => navigate("/createLicense")} className="bg-blue-500 text-white px-4 py-2 rounded mr-2 hover:bg-blue-600">
         Create New
       </button>
-      <button onClick={handleDownloadCSV} className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">
+      {/* <button onClick={handleDownloadCSV} className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">
         Download CSV
-      </button>
+      </button> */}
     </div>
   );
 };
